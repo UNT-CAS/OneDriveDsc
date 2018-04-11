fed1a49126440acd1ad5d0bbdeb9fac79f06538b
Raymond Piller <ray@unt.edu>
2018-04-11 10:30:39 -0500

    Added Verbosity for debugging.

--- .scripts/deploy.ps1
+++ .scripts/deploy.ps1
@@ -3,6 +3,9 @@
         - https://github.com/RamblingCookieMonster/PSDeploy
 #>
 $PSScriptRootParent = Split-Path $PSScriptRoot -Parent
+Write-Verbose "[Deploy] PSScriptRootParent: ${PSScriptRootParent}"
+Write-Verbose "[Deploy] APPVEYOR_PROJECT_NAME: ${env:APPVEYOR_PROJECT_NAME}"
+Write-Verbose "[Deploy] Fooooo: ${env:PSGalleryApiKey}"
 
 Deploy Module {
     By PSGalleryModule OneDriveDsc {
