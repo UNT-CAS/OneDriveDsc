d2557a64f1f6b1110b9c8bd52d88e2c8e99ee85a
Raymond Piller <ray@unt.edu>
2018-04-11 10:31:22 -0500

    Too much Verbosity
    Build will be deleted.

--- .scripts/deploy.ps1
+++ .scripts/deploy.ps1
@@ -5,7 +5,6 @@
 $PSScriptRootParent = Split-Path $PSScriptRoot -Parent
 Write-Verbose "[Deploy] PSScriptRootParent: ${PSScriptRootParent}"
 Write-Verbose "[Deploy] APPVEYOR_PROJECT_NAME: ${env:APPVEYOR_PROJECT_NAME}"
-Write-Verbose "[Deploy] Fooooo: ${env:PSGalleryApiKey}"
 
 Deploy Module {
     By PSGalleryModule OneDriveDsc {
